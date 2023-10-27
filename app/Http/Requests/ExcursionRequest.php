591c8a29e74e11b053b7cf50b30644b7846aa2f9
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-27 15:38:02 +0400

    excursion request

---
+++ app/Http/Requests/ExcursionRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExcursionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:255',
+            'description' => 'nullable|string|max:3000',
+            'image' => 'required|mimes:jpeg,jpg,png,webp|max:2048',
+            'price' => 'required',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => 'Наименование',
+            'description' => 'Описание',
+            'image' => 'Изображение',
+            'price' => 'Цена',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
