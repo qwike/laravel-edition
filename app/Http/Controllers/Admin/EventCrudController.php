468abc01b16919fe1e79def65b91b21c2c0783f3
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-27 16:44:55 +0400

    events controller

---
+++ app/Http/Controllers/Admin/EventCrudController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\EventRequest;
+use App\Models\Event;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class EventCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class EventCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Event::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/event');
+        CRUD::setEntityNameStrings('Мероприятие', 'Мероприятия');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'date',
+                'label' => 'Дата Проведения',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date'
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date'
+            ],
+        ]); // set columns from db columns.
+
+        /**
+         * Columns can be defined using the fluent syntax:
+         * - CRUD::column('price')->type('number');
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(EventRequest::class);
+        CRUD::addFields([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'date',
+                'label' => 'Дата Проведения',
+                'type' => 'text',
+                'hint' => 'Например: сезон, месяц или конкретная дата (03.09.2030 или 4 Февраля 2030 г.)',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'upload',
+                'withMedia' => [
+                    'collection' => Event::COLLECTION_NAME_EVENT,
+                ],
+                'hint' => 'Формат: jpeg, jpg, png, webp. Максимальный размер: 2MB',
+            ],
+        ]); // set fields from db columns.
+
+        /**
+         * Fields can be defined using the fluent syntax:
+         * - CRUD::field('price')->type('number');
+         */
+    }
+
+    protected function setupShowOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'date',
+                'label' => 'Дата Проведения',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'image',
+                'withMedia' => [
+                    'collection' => Event::COLLECTION_NAME_EVENT,
+                ],
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date',
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date',
+            ],
+        ]);
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
