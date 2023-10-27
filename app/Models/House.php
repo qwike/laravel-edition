029d6efee4a670ee519b32474025b5ea5970df15
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-27 16:30:28 +0400

    Add files via upload

---
+++ app/Models/House.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class House extends Model implements HasMedia
+{
+    use CrudTrait;
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public const COLLECTION_NAME_HOUSE = 'house';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function getProductImage(): ?Media
+    {
+        return $this->getFirstMedia(self::COLLECTION_NAME_HOUSE);
+    }
+}
