f7265b7995278447444f04cc32bd9f6b11496415
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-27 15:41:27 +0400

    Add files via upload

---
+++ app/Http/Controllers/Admin/EntertainmentCrudController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\EntertainmentRequest;
+use App\Models\Entertainment;
+use App\Models\Product;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class EntertainmentCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class EntertainmentCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Entertainment::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/entertainment');
+        CRUD::setEntityNameStrings('Развлечение', 'Развлечения');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date'
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date'
+            ],
+        ]);
+
+        /**
+         * Columns can be defined using the fluent syntax:
+         * - CRUD::column('price')->type('number');
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(EntertainmentRequest::class);
+        CRUD::addFields([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'price',
+                'label' => 'Цена',
+                'type' => 'number',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'upload',
+                'withMedia' => [
+                    'collection' => Entertainment::COLLECTION_NAME_ENTERTAINMENT,
+                ],
+                'hint' => 'Формат: jpeg, jpg, png, webp. Максимальный размер: 2MB',
+            ],
+        ]); // set fields from db columns.
+
+        /**
+         * Fields can be defined using the fluent syntax:
+         * - CRUD::field('price')->type('number');
+         */
+    }
+
+    protected function setupShowOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'image',
+                'withMedia' => [
+                    'collection' => Product::COLLECTION_NAME_ENTERTAINMENT,
+                ],
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date',
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date',
+            ],
+        ]);
+    }
+
+
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
