328fac85e351f57af2aa6832db8b9b6b6d639968
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-27 15:54:14 +0400

    projects model

---
+++ app/Models/Project.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Project extends Model implements HasMedia
+{
+    use CrudTrait;
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public const COLLECTION_NAME_PROJECT = 'project';
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function getProductImage(): ?Media
+    {
+        return $this->getFirstMedia(self::COLLECTION_NAME_PROJECT);
+    }
+}
