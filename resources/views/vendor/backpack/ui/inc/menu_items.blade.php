d7f74bbb6466d64d45f661fe32a8e387a94ec010
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-27 15:57:07 +0400

    Add files via upload

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -5,4 +5,5 @@
 <x-backpack::menu-item title="Развлечения" icon="la la-smile" :link="backpack_url('entertainment')" />
 <x-backpack::menu-item title="Продукты" icon="la la-shopping-cart" :link="backpack_url('product')" />
 <x-backpack::menu-item title="Адреса" icon="la la-map-marker-alt" :link="backpack_url('address')" />
+<x-backpack::menu-item title="Проекты" icon="la la-dollar-sign" :link="backpack_url('project')" />
 <x-backpack::menu-item title='Logs' icon='la la-terminal' :link="backpack_url('log')" />
