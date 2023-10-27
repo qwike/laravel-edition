9e595d5bc8199aff8e615632a0d50608b05b1202
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-27 16:30:03 +0400

    Add files via upload

---
+++ app/Http/Requests/HouseRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HouseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:255',
+            'description' => 'nullable|string|max:3000',
+            'price' => 'required',
+            'image' => 'required|mimes:jpeg,jpg,png,webp|max:2048',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => 'Наименование',
+            'description' => 'Описание',
+            'price' => 'Цена',
+            'image' => 'Изображение',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
