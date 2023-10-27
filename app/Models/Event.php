6d9028aa1e62ead3e4dbeb4827d60190fbe01084
Ivan <106988585+qwike@users.noreply.github.com>
2023-10-27 16:44:05 +0400

    events model

---
+++ app/Models/Event.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Event extends Model implements HasMedia
+{
+    use CrudTrait;
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public const COLLECTION_NAME_EVENT = 'event';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+    ];
+
+    public function getProductImage(): ?Media
+    {
+        return $this->getFirstMedia(self::COLLECTION_NAME_EVENT);
+    }
+}
