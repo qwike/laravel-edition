a82de52d51f553d94f0fba628725b244c0f8692f
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-27 15:45:28 +0400

    Add files via upload

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,7 +1,8 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-item title="Адреса" icon="la la-map-marker-alt" :link="backpack_url('address')" />
-<x-backpack::menu-item title="Продукты" icon="la la-shopping-cart" :link="backpack_url('product')" />
 <x-backpack::menu-item title="Экскурсии" icon="la la-route" :link="backpack_url('excursion')" />
+<x-backpack::menu-item title="Развлечения" icon="la la-smile" :link="backpack_url('entertainment')" />
+<x-backpack::menu-item title="Продукты" icon="la la-shopping-cart" :link="backpack_url('product')" />
+<x-backpack::menu-item title="Адреса" icon="la la-map-marker-alt" :link="backpack_url('address')" />
 <x-backpack::menu-item title='Logs' icon='la la-terminal' :link="backpack_url('log')" />
