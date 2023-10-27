27af4a98ec94b2d844d3f91380d0bdd44f04e64b
maximkasvetofor <106871362+maximkasvetofor@users.noreply.github.com>
2023-10-27 16:28:55 +0400

    Add files via upload

---
+++ app/Http/Controllers/Admin/HouseCrudController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\HouseRequest;
+use App\Models\House;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class HouseCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class HouseCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\House::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/house');
+        CRUD::setEntityNameStrings('house', 'houses');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'price',
+                'label' => 'Цена',
+                'type' => 'number',
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date'
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date'
+            ],
+        ]);
+
+        /**
+         * Columns can be defined using the fluent syntax:
+         * - CRUD::column('price')->type('number');
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(HouseRequest::class);
+        CRUD::addFields([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'price',
+                'label' => 'Цена',
+                'type' => 'number',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'upload',
+                'withMedia' => [
+                    'collection' => House::COLLECTION_NAME_HOUSE,
+                ],
+                'hint' => 'Формат: jpeg, jpg, png, webp. Максимальный размер: 2MB',
+            ],
+        ]);
+
+        /**
+         * Fields can be defined using the fluent syntax:
+         * - CRUD::field('price')->type('number');
+         */
+    }
+
+    protected function setupShowOperation()
+    {
+        CRUD::setColumns([
+            [
+                'name' => 'name',
+                'label' => 'Наименование',
+                'type' => 'text',
+                'limit' => 120,
+            ],
+            [
+                'name' => 'description',
+                'label' => 'Описание',
+                'type' => 'textarea',
+            ],
+            [
+                'name' => 'price',
+                'label' => 'Цена',
+                'type' => 'number',
+            ],
+            [
+                'name' => 'image',
+                'label' => 'Изображение',
+                'type' => 'image',
+                'withMedia' => [
+                    'collection' => House::COLLECTION_NAME_HOUSE,
+                ],
+            ],
+            [
+                'name' => 'created_at',
+                'label' => 'Создано',
+                'type' => 'date',
+            ],
+            [
+                'name' => 'updated_at',
+                'label' => 'Отредактировано',
+                'type' => 'date',
+            ],
+        ]);
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
